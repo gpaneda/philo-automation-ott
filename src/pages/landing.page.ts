f53d097e41d7bcc6bf0801980df4b51283e00cd5
Gerry Paneda <gpaneda@gmail.com>
2025-01-12 13:19:39 -0800

    test: update landing page selectors to match current UI elements

--- src/pages/landing.page.ts
+++ src/pages/landing.page.ts
@@ -2,28 +2,32 @@ import { Browser } from 'webdriverio';
 import { BasePage } from './base.page';
 
 export class LandingPage extends BasePage {
-    private readonly LANDING_PAGE_ELEMENTS = 'android=resourceId("com.philo.philo:id/landing_page_elements")';
-    private readonly CHANNELS_LIST = 'android=resourceId("com.philo.philo:id/channels_list")';
-    private readonly EXPLORE_FREE_CHANNELS_BUTTON = 'android=resourceId("com.philo.philo:id/explore_free_channels_button")';
-    private readonly LOGIN_SCREEN = 'android=resourceId("com.philo.philo:id/login_screen")';
+    readonly LANDING_SCREEN = 'android=resourceId("com.philo.philo:id/landing_screen_page_1")';
+    readonly PHILO_LOGO = 'android=resourceId("com.philo.philo:id/philo_logo")';
+    readonly HEADER_TEXT = 'android=resourceId("com.philo.philo:id/header_text")';
+    readonly START_TRIAL_BUTTON = 'android=resourceId("com.philo.philo:id/get_started_button")';
+    readonly SIGN_IN_BUTTON = 'android=resourceId("com.philo.philo:id/get_started_sign_in_button")';
+    readonly SEE_ALL_CHANNELS = 'android=resourceId("com.philo.philo:id/see_more")';
 
-    constructor(driver: Browser) {
+    constructor(driver: Browser<'async'>) {
         super(driver);
     }
 
     async verifyLandingPageElements(): Promise<void> {
-        await this.verifyElementDisplayed(this.LANDING_PAGE_ELEMENTS);
+        await this.verifyElementDisplayed(this.LANDING_SCREEN);
+        await this.verifyElementDisplayed(this.PHILO_LOGO);
+        await this.verifyElementDisplayed(this.HEADER_TEXT);
     }
 
     async verifyChannelsDisplayed(): Promise<void> {
-        await this.verifyElementDisplayed(this.CHANNELS_LIST);
+        await this.verifyElementDisplayed(this.SEE_ALL_CHANNELS);
     }
 
     async pressExploreFreeChannelsButton(): Promise<void> {
-        await this.click(this.EXPLORE_FREE_CHANNELS_BUTTON);
+        await this.click(this.SEE_ALL_CHANNELS);
     }
 
     async verifyLoginScreenDisplayed(): Promise<void> {
-        await this.verifyElementDisplayed(this.LOGIN_SCREEN);
+        await this.verifyElementDisplayed(this.SIGN_IN_BUTTON);
     }
 } 
