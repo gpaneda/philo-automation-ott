890b37cf33a7a2da9af7733c486011d25eafb5a8
Gerry Paneda <gpaneda@gmail.com>
2025-01-28 15:56:29 -0800

    Added capabilities for Android TV commented out

--- src/config/capabilities.ts
+++ src/config/capabilities.ts
@@ -16,3 +16,17 @@ export const fireTVCapabilities = {
     'appium:adbExecTimeout': 180000,
     'appium:sessionCommandTimeout': 180000
 }; 
+
+// export const androidTVCapabilities = {
+//     platformName: 'Android',
+//     'appium:automationName': 'UiAutomator2',
+//     'appium:deviceName': 'Android TV',
+//     'appium:udid': `${process.env.ANDROID_TV_IP}:${process.env.ANDROID_TV_PORT}`,
+//     'appium:platformVersion': '11',
+//     'appium:appPackage': 'com.philo.philo.google',
+//     'appium:appActivity': 'com.philo.philo.ui.activity.MainTabbedActivity',
+//     'appium:noReset': true,
+//     'appium:newCommandTimeout': 120000,
+//     'appium:adbExecTimeout': 180000,
+//     'appium:sessionCommandTimeout': 180000
+// }; 
